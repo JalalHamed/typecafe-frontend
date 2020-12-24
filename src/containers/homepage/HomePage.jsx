db1c06ed0cdbec3f11840472f296745c28015fe3
Jalal Hamed <iamjalalhamed@gmail.com>
2020-12-25 02:31:08 +0330

    adds scroll down icon

--- src/containers/homepage/HomePage.jsx
+++ src/containers/homepage/HomePage.jsx
@@ -2,17 +2,21 @@ import React from "react";
 
 // Componenets
 import Topbar from "components/topbar/Topbar";
-import LoginRegister from "components/login&register/LoginRegister";
+import LoginRegister from "./loginregister/LoginRegister";
+import GoToWhatIsIt from "./loginregister/GoToWhatIsIt";
 
 // Design
 import "./homepage.scss";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="home-page-wrapper">
       <Topbar />
-      <div className="home-page-wrapper">
+      <div className="loginregister-section-wrapper">
         <LoginRegister />
+      </div>
+      <div className="gotowhatisit-section-wrapper">
+        <GoToWhatIsIt />
       </div>
     </div>
   );
