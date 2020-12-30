a1cb3ce2160f2a16513a0b811bc5f811c51d4ab2
Jalal Hamed <iamjalalhamed@gmail.com>
2020-12-31 01:29:23 +0330

    adds style and animation to sidebar items

--- src/containers/dashboard/sidebar/SideBar.jsx
+++ src/containers/dashboard/sidebar/SideBar.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 // Designs
 import "./sidebar.scss";
+import "components/ripple/ripple.scss";
+
+// Components
+import TouchRipple from "components/ripple/TouchRipple";
 
 const SideBar = ({ setPage, page }) => {
+  const ProjectsRippleRef = useRef();
+  const FinancialRippleRef = useRef();
+  const SettingsRippleRef = useRef();
+
   return (
     <div className="sidebar-wrapper">
       <div className="user-profile">
@@ -18,24 +26,45 @@ const SideBar = ({ setPage, page }) => {
             page === "projects" ? "_selected" : ""
           }`}
           onClick={() => setPage("projects")}
+          onMouseDown={e => {
+            ProjectsRippleRef.current.start(e);
+          }}
+          onMouseUp={() => {
+            ProjectsRippleRef.current.stop();
+          }}
         >
           پروژه ها
+          <TouchRipple ref={ProjectsRippleRef} />
         </div>
         <div
           className={`sidebar-items_item${
             page === "financial" ? "_selected" : ""
           }`}
           onClick={() => setPage("financial")}
+          onMouseDown={e => {
+            FinancialRippleRef.current.start(e);
+          }}
+          onMouseUp={() => {
+            FinancialRippleRef.current.stop();
+          }}
         >
           کیف پول
+          <TouchRipple ref={FinancialRippleRef} />
         </div>
         <div
           className={`sidebar-items_item${
             page === "settings" ? "_selected" : ""
           }`}
           onClick={() => setPage("settings")}
+          onMouseDown={e => {
+            SettingsRippleRef.current.start(e);
+          }}
+          onMouseUp={() => {
+            SettingsRippleRef.current.stop();
+          }}
         >
           تنظیمات
+          <TouchRipple ref={SettingsRippleRef} />
         </div>
       </div>
     </div>
